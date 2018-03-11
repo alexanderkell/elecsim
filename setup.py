3e3246db2cd0eec40df6c6c1b4f5a5b196352844
Alexander Kell <alexander@kell.es>
2018-03-11 17:45:32 +0000

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='elecsim',
+    version='1.0',
+    description='A package for simulating the impact of technology and policy on the electricity sector',
+    author='Alexander Kell',
+    author_email='alexander@kell.es',
+    packages=['elecsim'],
+    install_requires=['mesa'], # external packages as dependencies
+    long_description="A package to aid in the designing of an energy market of a country to see impacts of different regulatory and technological changes",
+)
