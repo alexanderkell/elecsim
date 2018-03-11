94603bd3f5f41720431db1e06b943c5407eb441b
Alexander Kell <alexander@kell.es>
2018-03-11 18:05:51 +0000

    Added url to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='A package for simulating the impact of technology and policy on the electricity sector',
     author='Alexander Kell',
     author_email='alexander@kell.es',
+    url='https://github.com/alexanderkell/elecsim',
     packages=['elecsim'],
     install_requires=['mesa'], # external packages as dependencies
     long_description="A package to aid in the designing of an energy market of a country to see impacts of different regulatory and technological changes",
