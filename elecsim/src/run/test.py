932ff0f492e98fa8a6a5ef609732a9e7d6cad3e3
Alexander Kell <alexander@kell.es>
2018-11-24 18:11:04 +0000

    Commented out test and test_functions to find best way of testing program

--- elecsim/src/run/test.py
+++ elecsim/src/run/test.py
@@ -1,14 +1,14 @@
-"""run.py: Module to run the model"""
-
-from elecsim.src.model.model import Model
-
-import elecsim.src.scenario.scenario_data as scenario
-
-model = Model(scenario)
-
-for i in range(2):
-    model.step()
-
-
-
-
+# """run.py: Module to run the model"""
+#
+# from elecsim.src.model.model import Model
+#
+# import elecsim.src.scenario.scenario_data as scenario
+#
+# model = Model(scenario)
+#
+# for i in range(2):
+#     model.step()
+#
+#
+#
+#
