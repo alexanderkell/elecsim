2f0a27877e92f7d4b08a791be18a2b789db493e0
Alexander Kell <alexander@kell.es>
2018-12-04 16:19:19 +0000

    Added src. to beginning of fuel_plants_old_params import FuelOldPlantCosts

--- src/plants/plant_costs/estimate_costs/estimate_old_plant_cost_params/fuel_plant_calculations/fuel_plants_old_params.py
+++ src/plants/plant_costs/estimate_costs/estimate_old_plant_cost_params/fuel_plant_calculations/fuel_plants_old_params.py
@@ -1,7 +1,7 @@
 from scipy.optimize import minimize
 
 import src.scenario.scenario_data as scenario
-from plants.plant_type.fuel_plants.fuel_plant import FuelPlant
+from src.plants.plant_type.fuel_plants.fuel_plant import FuelPlant
 from src.plants.fuel.fuel_registry.fuel_registry import plant_type_to_fuel
 from src.plants.plant_costs.estimate_costs.estimate_old_plant_cost_params.old_plant_param_calc import OldPlantCosts
 
