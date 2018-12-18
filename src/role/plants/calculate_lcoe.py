c46eb2cf0ca3a75dac3ef0d8295725a3f8042b16
Alexander Kell <alexander@kell.es>
2018-12-18 11:27:30 +0000

    Added calculate_lcoe file. Begun working on moving LCOE calculations into here

---
+++ src/role/plants/calculate_lcoe.py
@@ -0,0 +1,10 @@
+"""
+File name: calculate_lcoe
+Date created: 18/12/2018
+Feature: #Enter feature description here
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
