cde5d100ff6d542c2fa4d946409a8523a94c4e91
Alexander Kell <alexander@kell.es>
2018-12-24 22:28:16 +0000

    Begun working on NPV calculation

---
+++ src/role/investment/calculate_npv.py
@@ -0,0 +1,21 @@
+from numpy import npv
+
+from src.role.plants.fuel_lcoe_calculation import FuelLCOECalculation
+
+"""
+File name: calculate_npv
+Date created: 24/12/2018
+Feature: # Contains functionality to assess options of investment and return lowest NPV for decision to be made.
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
+
+
+def calculate_npv(discount_rate):
+
+    FuelLCOECalculation
+
+    npv(discount_rate, )
---
+++ test/test_investment/test_calculate_npv.py
@@ -0,0 +1,18 @@
+
+from src.role.investment.calculate_npv import calculate_npv
+
+"""
+File name: test_calculate_npv
+Date created: 24/12/2018
+Feature: #Enter feature description here
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
+
+
+class TestCalculate_npv:
+    def test_calculate_npv(self):
+        calculate_npv(0.05)
