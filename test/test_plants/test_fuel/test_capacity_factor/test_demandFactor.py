30a1ca7fbc9f5672521e3602b8ffa1686d661562
Alexander Kell <alexander@kell.es>
2018-12-27 21:06:14 +0000

    Added tests for demand factor calculations

--- test/test_plants/test_fuel/test_capacity_factor/test_demandFactor.py
+++ test/test_plants/test_fuel/test_capacity_factor/test_demandFactor.py
@@ -1,5 +1,7 @@
 from src.plants.fuel.capacity_factor.capacity_factor_calculations import DemandFactor
+import pytest
 import logging
+logger = logging.getLogger(__name__)
 """
 File name: test_demandFactor
 Date created: 27/12/2018
@@ -16,6 +18,17 @@ __email__ = "alexander@kell.es"
 
 
 class TestDemandFactor(TestCase):
-    def test_calculate_demand_factor_for_segment(self):
+    def test_calculate_pv_demand_factor_for_segment(self):
         demand_factor = DemandFactor("pv")
-        demand_factor.calculate_demand_factor_for_segment()
+        demand_factors = demand_factor.calculate_demand_factor_for_segment()
+        assert demand_factors.mean() ==  pytest.approx(0.1053, abs=0.01)
+
+    def test_calculate_onshore_demand_factor_for_segment(self):
+        demand_factor = DemandFactor("onshore")
+        demand_factors = demand_factor.calculate_demand_factor_for_segment()
+        assert demand_factors.mean() == pytest.approx(0.3, abs=0.5)
+
+    def test_calculate_offshore_demand_factor_for_segment(self):
+        demand_factor = DemandFactor("offshore")
+        demand_factors = demand_factor.calculate_demand_factor_for_segment()
+        assert demand_factors.mean() == pytest.approx(0.39, abs=1)
