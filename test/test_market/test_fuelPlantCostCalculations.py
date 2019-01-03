a4a917c9a7c50bf22ad95be3f7e33f76ba553392
Alexander Kell <alexander@kell.es>
2019-01-03 16:08:13 +0000

    Fixed test_fuelPlantCost calculation tests of SRMC

--- test/test_market/test_fuelPlantCostCalculations.py
+++ test/test_market/test_fuelPlantCostCalculations.py
@@ -39,15 +39,14 @@ class TestFuelPlantCostCalculations:
                  variable_o_and_m_per_mwh = 3, insurance_cost_per_mw = 2100, connection_cost_per_mw = 3300)
         logger.debug(plant_cost_calculation.calculate_short_run_marginal_cost(mock_model))
         short_run_marginal_cost = plant_cost_calculation.calculate_short_run_marginal_cost(mock_model)
-        assert short_run_marginal_cost == pytest.approx(28.1362533)
+        assert short_run_marginal_cost == pytest.approx(37.3842440)
 
     def test_short_run_marginal_cost_nuclear(self, mock_model):
         mock_model.year_number=2018
-        # assert mock_model.year_number == 2018
         plant_cost_calculation = FuelPlantCostCalculations(plant_type = "Nuclear", capacity_mw = 1200, construction_year = 2018, average_load_factor = 0.93, efficiency = 0.54, pre_dev_period = 3,
                  construction_period = 3, operating_period = 25, pre_dev_spend_years = [0.44, 0.44, 0.12], construction_spend_years = [0.4, 0.4, 0.2],
                  pre_dev_cost_per_mw = 10000, construction_cost_per_mw = 500000, infrastructure = 15100000, fixed_o_and_m_per_mw = 12200,
                  variable_o_and_m_per_mwh = 3, insurance_cost_per_mw = 2100, connection_cost_per_mw = 3300)
         logger.debug(plant_cost_calculation.calculate_short_run_marginal_cost(mock_model))
         short_run_marginal_cost = plant_cost_calculation.calculate_short_run_marginal_cost(mock_model)
-        assert short_run_marginal_cost == pytest.approx(28.1362533)
+        assert short_run_marginal_cost == pytest.approx(10.22222222)
