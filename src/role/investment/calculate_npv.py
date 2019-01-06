66c8ab34675474f47b4c67c18bd7c1eb91eadb86
Alexander Kell <alexander@kell.es>
2019-01-06 10:05:57 +0000

    Fixed discount rate to equal WACC.

--- src/role/investment/calculate_npv.py
+++ src/role/investment/calculate_npv.py
@@ -38,8 +38,8 @@ class CalculateNPV:
     def compare_npv(self):
         cost_list = []
 
-        # for plant_type in ['CCGT','Coal','Nuclear','Onshore', 'Offshore', 'PV']:
-        for plant_type in ['Nuclear']:
+        for plant_type in ['CCGT','Coal','Nuclear','Onshore', 'Offshore', 'PV']:
+        # for plant_type in ['Nuclear']:
 
             plant_cost_data = modern_plant_costs[modern_plant_costs.Type==plant_type]
             for plant_row in plant_cost_data.itertuples():
@@ -96,10 +96,12 @@ class CalculateNPV:
 
         if power_plant.plant_type=="Nuclear":
             self.weighted_average_cost_capital = nuclear_wacc
+            self.discount_rate = nuclear_wacc
         else:
             self.weighted_average_cost_capital = non_nuclear_wacc
+            self.discount_rate = non_nuclear_wacc
 
-        # self.weighted_average_cost_capital = 0
+        self.weighted_average_cost_capital = 0
 
         # for i in range(power_plant.pre_dev_period+power_plant.construction_period+power_plant.operating_period):
         cash_flow_wacc = [cash_flow/(1+self.weighted_average_cost_capital)**counter for counter, cash_flow in enumerate(total_cash_flow,1)]
@@ -111,7 +113,7 @@ class CalculateNPV:
 
         logger.debug("npv_power_plant: {}".format(npv_power_plant))
 
-        NPVp = npv_power_plant/(power_plant.capacity_mw)
+        NPVp = npv_power_plant/(power_plant.capacity_mw*365*24)
         return NPVp
 
     def _get_yearly_profit_per_mwh(self, power_plant, total_running_hours, yearly_cash_flow):
