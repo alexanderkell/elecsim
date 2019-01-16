e428af620c4245ae190604db942152ce1fc4e169
Alexander Kell <alexander@kell.es>
2019-01-16 22:16:13 +0000

    Running investment algorithm.

--- test/test_model/test_world.py
+++ test/test_model/test_world.py
@@ -20,13 +20,13 @@ __copyright__ = "Copyright 2018, Alexander Kell"
 __license__ = "MIT"
 __email__ = "alexander@kell.es"
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-class TestWorld:
-    def test_world_initialization(self):
+# class TestWorld:
+#     def test_world_initialization(self):
 # with PyCallGraph(output=GraphvizOutput()):
-        world = World(scenario=scenario_data, initialization_year=2018)
+world = World(scenario=scenario_data, initialization_year=2018)
 
-        for i in range(1):
-            world.step()
+for i in range(32):
+    world.step()
 
