d18235aa34337a2de0b8d8ff035d07c219a43237
Alexander Kell <alexander@kell.es>
2019-01-20 15:58:04 +0000

    added directories for plotting graphs of scenarios

---
+++ run/batchrunners/scenarios/graph_plotter.py
@@ -0,0 +1,10 @@
+"""
+File name: graph_plotter
+Date created: 20/01/2019
+Feature: #Enter feature description here
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
