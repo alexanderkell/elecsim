6166825c2424e7f308bb33aef26f323039478174
Ubuntu <alexkell@elecsim-64.tpaa2mnznnheloftvrbuex2ffg.xx.internal.cloudapp.net>
2019-01-23 19:23:57 +0000

    Full run of debugged code - Half finished

--- run/batchrunners/batch_run.py
+++ run/batchrunners/batch_run.py
@@ -32,24 +32,24 @@ __email__ = "alexander@kell.es"
 
 if __name__ == '__main__':
     data_folder = sys.argv[0]
-    number_of_steps = 1
+    number_of_steps = 38
 
-    fixed_params = {"initialization_year": 2018,
+    fixed_params = {"initialization_year": 2013,
                     "number_of_steps": number_of_steps,
                     "data_folder": data_folder}
 
     # variable_params = {"carbon_price_scenario": [[20]*50,[30]*50, [40]*50, [45]*50 ,[50]*50, [55]*50,[65]*50, [75]*50, [85]*50,[95]*50, [100]*50, [200]*50, list(range(17, 177, 4)) , list(range(101, 21, -2))],
     #               "demand_change": [[1.01]*50, [0.990]*50]
     #                }
-    variable_params = {"carbon_price_scenario": [[30]*50],
-                  "demand_change": [[1.01]*50]
+    variable_params = {"carbon_price_scenario": [[0]*50, [10]*50 ,[20]*50, [30]*50, [40]*50, [50]*50, [60]*50, [70]*50, [80]*50,[90]*50, [100]*50, [150]*50,[200]*50 ,list(range(18, 170, 4)), list(range(170, 18, -4)) ],
+                  "demand_change": [[1.01]*50, [0.99]*50]
                    }
 
     batch_run = BatchRunnerMP(World,
                           fixed_parameters=fixed_params,
                           variable_parameters=variable_params,
-                          iterations=1,
-                          max_steps=number_of_steps, nr_processes=3)
+                          iterations=2,
+                          max_steps=number_of_steps, nr_processes=63)
 
     batch_run.run_all()
 
