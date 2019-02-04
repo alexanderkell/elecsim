8fa7b3b819f2b25e694609f74df9dc9ac3261591
Alexander Kell <alexander@kell.es>
2019-02-04 22:49:54 +0000

    Final draft. Waiting for memory profiling to run

--- test/test_model/test_world.py
+++ test/test_model/test_world.py
@@ -5,8 +5,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from src.model.world import World
 from src.scenario import scenario_data
 import logging
+import tracemalloc
 
 import pandas as pd
+import linecache
 
 from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
@@ -27,14 +29,44 @@ __email__ = "alexander@kell.es"
 
 pd.set_option('display.max_rows', 4000)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
-# class TestWorld:
-#     def test_world_initialization(self):
-with PyCallGraph(output=GraphvizOutput()):
+
+def display_top(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+class TestWorld:
+    def test_world_initialization(self):
+# with PyCallGraph(output=GraphvizOutput()):
         world = World(initialization_year=2018)
+        tracemalloc.start()
 
         for i in range(10):
+            snapshot = tracemalloc.take_snapshot()
+            display_top(snapshot)
+
             world.step()
 
         # data = world.datacollector.get_model_vars_dataframe()
