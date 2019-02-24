7a247c4d8824e94e261c695e077416d8a92eb3d7
Alexander Kell <alexander@kell.es>
2019-02-24 21:21:34 +0000

    fixed absolute path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../elecsim'))
+sys.path.insert(0, os.path.abspath('../elecsim/'))
 
 
 # -- Project information -----------------------------------------------------
