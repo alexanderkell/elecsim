3b6036b58a0141ebf0eaf2032276492b8b691380
Alexander Kell <alexander@kell.es>
2019-02-25 13:45:39 +0000

    added favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,6 +99,9 @@ html_theme = 'default'
 #
 # html_theme_options = {}
 
+
+html_logo = "sitelogo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
