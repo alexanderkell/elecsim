11f308155a2aa24765252ae10527333471b6bb67
Alexander Kell <alexander@kell.es>
2019-03-01 09:32:09 +0000

    added imports to init file of elecsim

--- test/test_model/test_world.py
+++ test/test_model/test_world.py
@@ -26,43 +26,43 @@ pd.set_option('display.max_rows', 4000)
 
 logging.basicConfig(level=logging.WARNING)
 
-
-def display_top(snapshot, key_type='lineno', limit=3):
-    snapshot = snapshot.filter_traces((
-        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-        tracemalloc.Filter(False, "<unknown>"),
-    ))
-    top_stats = snapshot.statistics(key_type)
-
-    print("Top %s lines" % limit)
-    for index, stat in enumerate(top_stats[:limit], 1):
-        frame = stat.traceback[0]
-        # replace "/path/to/module/file.py" with "module/file.py"
-        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
-        print("#%s: %s:%s: %.1f KiB"
-              % (index, filename, frame.lineno, stat.size / 1024))
-        line = linecache.getline(frame.filename, frame.lineno).strip()
-        if line:
-            print('    %s' % line)
-
-    other = top_stats[limit:]
-    if other:
-        size = sum(stat.size for stat in other)
-        print("%s other: %.1f KiB" % (len(other), size / 1024))
-    total = sum(stat.size for stat in top_stats)
-    print("Total allocated size: %.1f KiB" % (total / 1024))
-
-class TestWorld:
-    def test_world_initialization(self):
-# with PyCallGraph(output=GraphvizOutput()):
-        world = World(initialization_year=2018)
-        tracemalloc.start()
-
-        for i in range(10):
-            snapshot = tracemalloc.take_snapshot()
-            display_top(snapshot)
-
-            world.step()
-
-        # data = world.datacollector.get_model_vars_dataframe()
-        # logger.info("final data: \n {}".format(data))
+#
+# def display_top(snapshot, key_type='lineno', limit=3):
+#     snapshot = snapshot.filter_traces((
+#         tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+#         tracemalloc.Filter(False, "<unknown>"),
+#     ))
+#     top_stats = snapshot.statistics(key_type)
+#
+#     print("Top %s lines" % limit)
+#     for index, stat in enumerate(top_stats[:limit], 1):
+#         frame = stat.traceback[0]
+#         # replace "/path/to/module/file.py" with "module/file.py"
+#         filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+#         print("#%s: %s:%s: %.1f KiB"
+#               % (index, filename, frame.lineno, stat.size / 1024))
+#         line = linecache.getline(frame.filename, frame.lineno).strip()
+#         if line:
+#             print('    %s' % line)
+#
+#     other = top_stats[limit:]
+#     if other:
+#         size = sum(stat.size for stat in other)
+#         print("%s other: %.1f KiB" % (len(other), size / 1024))
+#     total = sum(stat.size for stat in top_stats)
+#     print("Total allocated size: %.1f KiB" % (total / 1024))
+#
+# class TestWorld:
+#     def test_world_initialization(self):
+# # with PyCallGraph(output=GraphvizOutput()):
+#         world = World(initialization_year=2018)
+#         tracemalloc.start()
+#
+#         for i in range(10):
+#             snapshot = tracemalloc.take_snapshot()
+#             display_top(snapshot)
+#
+#             world.step()
+#
+#         # data = world.datacollector.get_model_vars_dataframe()
+#         # logger.info("final data: \n {}".format(data))
