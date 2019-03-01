f5fd1621834fc3d1a8676d7594368e7ae6ba2579
Alexander Kell <alexander@kell.es>
2019-03-01 09:39:25 +0000

    changed setup file for newest release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Alexander Kell',                   # Type in your name
   author_email = 'alexander@kell.es',      # Type in your E-Mail
   url = 'https://github.com/alexanderkell/elecsim',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/alexanderkell/elecsim/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/alexanderkell/elecsim/archive/0.1.1.1.tar.gz',    # I explain this later on
   keywords = ['ElecSIM', 'Agent-based', 'Electricity', 'Market', 'Model', 'Simulation'],   # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'mesa',
