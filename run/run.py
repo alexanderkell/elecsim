154142731402ec50592bd96a3e64f2f992e02626
Alexander Kell <alexander@kell.es>
2019-03-03 12:23:10 +0000

    Added docstring to World class. Added logging level user input

---
+++ run/run.py
@@ -0,0 +1,14 @@
+import sys
+# import scenario_file
+# sys.modules['elecsim'].scenario.scenario_data=scenario_file
+
+from elecsim.model.world import World
+
+import logging
+# logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    world = World(2018, log_level="info")
+    # for i in range(20):
+        # world.step()
