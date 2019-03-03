d578da5324d86c2bc39962fa27295076bde0ec44
Alexander Kell <alexander@kell.es>
2019-03-03 21:19:01 +0000

    removed rllib as a dependency as does not work on windows

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
   package_data={'elecsim': extra_files},
 
   py_modules=['elecsim'],
-  version = '0.1.19',
+  version = '0.1.20',
   license='MIT',
   description = 'Agent-based Model for Electricity Markets',
   author = 'Alexander Kell',
