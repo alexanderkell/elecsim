e0a23247d28d85f2c9a4f364dbcd2c1c39e63a04
Alexander Kell <alexander@kell.es>
2019-03-05 17:43:29 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
   package_data={'elecsim': extra_files},
 
   py_modules=['elecsim'],
-  version = '0.1.24',
+  version = '0.1.25',
   license='MIT',
   description = 'Agent-based Model for Electricity Markets',
   author = 'Alexander Kell',
