9b19bdd407899dfb93680a6597f9757b1aca850a
Alexander Kell <alexander@kell.es>
2019-03-06 11:45:32 +0000

    updated linear regression to handle errors

--- elecsim/data_manipulation/data_modifications/linear_regression.py
+++ elecsim/data_manipulation/data_modifications/linear_regression.py
@@ -1,5 +1,5 @@
 from scipy.stats import linregress
-
+import numpy as np
 """
 File name: linear_regression
 Date created: 29/12/2018
@@ -13,7 +13,17 @@ __email__ = "alexander@kell.es"
 
 def linear_regression(regression, years_to_look_back, years_to_look_ahead=None):
     years_to_regress = list(range(years_to_look_back))
-    years_to_regress = [float(i) for i in years_to_regress]
-    m, c, _, _, _ = linregress(years_to_regress, regression)
-    next_value = m * (years_to_look_back+years_to_look_ahead-1) + c
+    years_to_regress = np.array(years_to_regress).astype(float)
+    try:
+        regression = np.stack(regression, axis=1)
+    except:
+        pass
+    try:
+        m, c, _, _, _ = linregress(years_to_regress, np.array(regression).astype(float))
+        next_value = m * (years_to_look_back+years_to_look_ahead-1) + c
+    except Exception as e:
+        print(str(e.with_traceback))
+        print("regression: {}".format(regression))
+        print("years to regress: {}".format(years_to_regress))
+        next_value = regression[-1]
     return next_value
