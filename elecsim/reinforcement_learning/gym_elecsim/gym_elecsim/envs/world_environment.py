3a707c160f94bedc9d4cee9ab3b13f0054180531
Alexander Kell <alexander@kell.es>
2019-03-10 08:58:59 +0000

    updated world environment

--- elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
+++ elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
@@ -25,12 +25,14 @@ from gym.utils import seeding
 
 class WorldEnvironment(gym.Env):
 
-    def __init__(self, scenario_file=None, max_number_of_steps=32, data_folder="reinforcement_learning"):
+    # def __init__(self, scenario_file=None, max_number_of_steps=32, data_folder="reinforcement_learning"):
+    def __init__(self, config):
         print("trying to init")
         self.step_number = 0
         self.action = None
-        self.max_number_of_steps = max_number_of_steps
-        self.world = World(initialization_year=2018, scenario_file=scenario_file, data_folder=data_folder, number_of_steps=self.max_number_of_steps)
+        self.config = config
+        self.max_number_of_steps = config['max_number_of_steps']
+        self.world = World(initialization_year=2018, scenario_file=config['scenario_file'], data_folder=config['data_folder'], number_of_steps=self.max_number_of_steps)
         self.action_space = Box(
             0.0, 250.0, shape=(1, ), dtype=np.float32)
 
@@ -38,22 +40,21 @@ class WorldEnvironment(gym.Env):
             0.0, lost_load, shape=(1, ), dtype=np.float32)
 
     def reset(self):
-        self.world = World(initialization_year=2018)
-        return [0]
+        self.world = World(initialization_year=2018, scenario_file=self.config['scenario_file'], data_folder=self.config['data_folder'], number_of_steps=self.max_number_of_steps)
+        return np.expand_dims(np.array(0), axis=0)
 
     def step(self, action):
-        print("stepping number: {}".format(self.world.step_number))
         self.action = action
         self.step_number += 1
-        ob = [self.world.step(action)]
-        reward = ob[0]
+        ob = self.world.step(action)
+        reward = ob
 
         done = self.world.step_number > self.max_number_of_steps
 
-        return np.array(ob), reward, done, {}
+        return np.expand_dims(np.array(ob), axis=0), reward, done, {}
         # return np.array([1]), reward, done, {}
 
-    def render(self, mode='human', close=False):
+    def render(self):
         print("num steps {}".format(self.step_number))
         print("action: {}".format(self.action))
 
