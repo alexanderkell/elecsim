fdf92dfe9bd4d4d8736664985280a52273081ea0
Alexander Kell <alexander@kell.es>
2019-03-10 12:18:04 +0000

    updated world_environment to have multiple observations

--- elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
+++ elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
@@ -37,25 +37,38 @@ class WorldEnvironment(gym.Env):
             0.0, 250.0, shape=(1, ), dtype=np.float32)
 
         self.observation_space = Box(
-            0.0, lost_load, shape=(1, ), dtype=np.float32)
+            0.0, lost_load, shape=(2, ), dtype=np.float32)
 
     def reset(self):
         self.world = World(initialization_year=2018, scenario_file=self.config['scenario_file'], data_folder=self.config['data_folder'], number_of_steps=self.max_number_of_steps)
-        return np.expand_dims(np.array(0), axis=0)
+        obs = np.random.uniform(low=-100, high=1, size=(2,))
+        obs = np.array(obs)
+        # return np.expand_dims(obs, axis=0)
+        return obs
 
     def step(self, action):
         self.action = action
         self.step_number += 1
-        ob = self.world.step(action)
+        mean_electricity_price = self.world.step(action)[0]
+        carbon_emitted = self.world.step(action)[1]
+
+        ob = -abs(mean_electricity_price)-abs(carbon_emitted)
         reward = ob
 
         done = self.world.step_number > self.max_number_of_steps
+        print("step: {}".format(self.step_number))
+        print("action: {}, reward: {}, mean_electricity_price: {}, carbon_emitted: {}".format(action, reward, mean_electricity_price, carbon_emitted))
 
-        return np.expand_dims(np.array(ob), axis=0), reward, done, {}
-        # return np.array([1]), reward, done, {}
+        ob = np.array([mean_electricity_price, carbon_emitted])
+
+
+        return ob, reward, done, {}
+        # return ob.reshape(2,1), reward, done, {}
+        # return np.expand_dims(np.array(ob), axis=0), reward, done, {}
+        # return np.array(ob).reshape(2,1), reward, done, {}
 
     def render(self):
         print("num steps {}".format(self.step_number))
         print("action: {}".format(self.action))
 
-        return False
+        return True
