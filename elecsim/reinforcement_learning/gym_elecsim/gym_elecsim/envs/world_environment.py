bf623115c4face37b69fe429da442d3c979ce04a
Alexander Kell <alexander@kell.es>
2019-03-12 11:41:40 +0000

    removed cumulative reward

--- elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
+++ elecsim/reinforcement_learning/gym_elecsim/gym_elecsim/envs/world_environment.py
@@ -28,7 +28,6 @@ class WorldEnvironment(gym.Env):
     # def __init__(self, scenario_file=None, max_number_of_steps=32, data_folder="reinforcement_learning"):
     def __init__(self, config):
         print("trying to init")
-        self.reward = 0
         self.step_number = 0
         self.action = None
         self.config = config
@@ -41,6 +40,7 @@ class WorldEnvironment(gym.Env):
             0.0, lost_load, shape=(2, ), dtype=np.float32)
 
     def reset(self):
+        self.step_number=0
         self.world = World(initialization_year=2018, scenario_file=self.config['scenario_file'], data_folder=self.config['data_folder'], number_of_steps=self.max_number_of_steps)
         obs = np.random.uniform(low=-100, high=1, size=(2,))
         obs = np.array(obs)
@@ -54,16 +54,16 @@ class WorldEnvironment(gym.Env):
         carbon_emitted = self.world.step(action)[1]
 
         ob = -abs(mean_electricity_price)-abs(carbon_emitted)
-        self.reward = ob+0.9*self.reward
+        reward = ob
 
         done = self.world.step_number > self.max_number_of_steps
         print("step: {}".format(self.step_number))
-        print("action: {}, reward: {}, mean_electricity_price: {}, carbon_emitted: {}".format(action, self.reward, mean_electricity_price, carbon_emitted))
+        print("action: {}, reward: {}, mean_electricity_price: {}, carbon_emitted: {}".format(action, reward, mean_electricity_price, carbon_emitted))
 
         ob = np.array([mean_electricity_price, carbon_emitted])
 
 
-        return ob, self.reward, done, {}
+        return ob, reward, done, {}
         # return ob.reshape(2,1), reward, done, {}
         # return np.expand_dims(np.array(ob), axis=0), reward, done, {}
         # return np.array(ob).reshape(2,1), reward, done, {}
