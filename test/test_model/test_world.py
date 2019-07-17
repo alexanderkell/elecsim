13d18a22c99684151d93343f53119854c949339c
Alexander Kell <alexander@kell.es>
2019-07-17 16:12:17 +0100

    began working on reinforcement learning investment algorithm

--- test/test_model/test_world.py
+++ test/test_model/test_world.py
@@ -58,7 +58,7 @@ logging.basicConfig(level=logging.INFO)
 
 # with PyCallGraph(output=GraphvizOutput()):
 MARKET_TIME_SPLICES = 10
-YEARS_TO_RUN = 10
+YEARS_TO_RUN = 50
 number_of_steps = YEARS_TO_RUN * MARKET_TIME_SPLICES
 world = World(initialization_year=2018, market_time_splices=MARKET_TIME_SPLICES, data_folder="test_new", number_of_steps=number_of_steps)
 
