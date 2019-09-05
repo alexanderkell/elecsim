e9dce428dba5cbfa7c36a0d03d86de8c5d130f0c
Alexander Kell <alexander@kell.es>
2019-09-05 18:08:02 +0100

    added optimiser for BEIS strategy

---
+++ run/beis_case_study/optimisation/optimiser_for_beis_scenario.py
@@ -0,0 +1,10 @@
+"""
+File name: optimiser_for_beis_scenario
+Date created: 04/09/2019
+Feature: #Enter feature description here
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
