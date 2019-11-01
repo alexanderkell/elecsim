3a3778bbde4125d45b258870eabea12b991e82ce
Alexander Kell <alexander@kell.es>
2019-11-01 08:16:30 +0000

    fixed bug

--- run/beis_case_study/run/run_individual_beis_comparer.py
+++ run/beis_case_study/run/run_individual_beis_comparer.py
@@ -25,17 +25,17 @@ __copyright__ = "Copyright 2018, Alexander Kell"
 __license__ = "MIT"
 __email__ = "alexander@kell.es"
 
-ga_results = pd.read_csv('run/beis_case_study/data/GA_optimisation_results/GA_results.csv')
+# ga_results = pd.read_csv('run/beis_case_study/data/GA_optimisation_results/GA_results.csv')
 # ga_results = pd.read_csv('/Users/b1017579/Documents/PhD/Projects/10. ELECSIM/run/beis_case_study/data/GA_optimisation_results/GA_results.csv')
 
-ga_results_small = ga_results[ga_results.reward < 10].iloc[:,7:-5]
-params_list = ga_results_small.values.tolist()
-
-
-params_repeated = np.repeat(params_list, 100, axis=0)
-
-params_repeated_list = params_repeated.tolist()
-
+# ga_results_small = ga_results[ga_results.reward < 10].iloc[:,7:-5]
+# params_list = ga_results_small.values.tolist()
+#
+#
+# params_repeated = np.repeat(params_list, 100, axis=0)
+#
+# params_repeated_list = params_repeated.tolist()
+#
 # params_repeated_list = np.array([0]*74).reshape(2,-1).tolist()
 
 
@@ -74,8 +74,8 @@ def eval_world_parallel(individual):
 #     pickle.dump(results, f)
 
 
-# params_repeated_list = np.repeat([[0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.0021988838824299997, 12.633572943294599, 120.21276910611674, 0.0, 0.00059945111227]]
-#                                  , 50, axis=0)
+params_repeated_list = np.repeat([[0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.0021988838824299997, 12.633572943294599, 120.21276910611674, 0.0, 0.00059945111227]]
+                                 , 50, axis=0)
 
 pool = Pool(cpu_count())
 out1, out2 = zip(*pool.map(eval_world_parallel, params_repeated_list))
