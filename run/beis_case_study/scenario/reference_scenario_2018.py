9e46681e4d8c635343dcbe79867e4b2c594f73c8
Alexander Kell <alexander@kell.es>
2020-02-03 17:59:07 +0000

    working on papers

--- run/beis_case_study/scenario/reference_scenario_2018.py
+++ run/beis_case_study/scenario/reference_scenario_2018.py
@@ -107,10 +107,10 @@ non_nuclear_wacc = 0.059  # https://assets.kpmg/content/dam/kpmg/ch/pdf/cost-of-
 
 # Availability
 non_fuel_plant_availability = 0.97
-pv_availability = 0.995 # https://ieeexplore.ieee.org/document/7355976
-offshore_availability = 0.95 # https://pureportal.strath.ac.uk/files-asset/43185998/Carroll_etal_EWEA2015_availability_improvements_from_condition_monitoring_systems.pdf
-onshore_availability = 0.97 # https://pureportal.strath.ac.uk/files-asset/43185998/Carroll_etal_EWEA2015_availability_improvements_from_condition_monitoring_systems.pdf
-fuel_plant_availability = 0.97 # Electricity Generation Costs and Hurdle Rates - Leigh_Fisher_Non-renewable_Generation_Cost.pdf
+pv_availability = 0.995  # https://ieeexplore.ieee.org/document/7355976
+offshore_availability = 0.95  # https://pureportal.strath.ac.uk/files-asset/43185998/Carroll_etal_EWEA2015_availability_improvements_from_condition_monitoring_systems.pdf
+onshore_availability = 0.97  # https://pureportal.strath.ac.uk/files-asset/43185998/Carroll_etal_EWEA2015_availability_improvements_from_condition_monitoring_systems.pdf
+fuel_plant_availability = 0.97  # Electricity Generation Costs and Hurdle Rates - Leigh_Fisher_Non-renewable_Generation_Cost.pdf
 
 
 # Capacity factor data (from https://www.renewables.ninja/)
