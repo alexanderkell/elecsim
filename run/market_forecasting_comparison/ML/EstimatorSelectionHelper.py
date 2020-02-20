a15a76679d4aee93ef8e55532231845078e4ee7d
Alexander Kell <alexander@kell.es>
2020-02-20 21:52:04 +0000

    improved parallelism

--- run/market_forecasting_comparison/ML/EstimatorSelectionHelper.py
+++ run/market_forecasting_comparison/ML/EstimatorSelectionHelper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import ray
 
 from sklearn.model_selection import GridSearchCV
 
@@ -42,13 +43,28 @@ class EstimatorSelectionHelper:
 
             pipeline = self.make_pipeline(model)
             params = self.params[key]
-            tscv = TimeSeriesSplit(n_splits=cv)
+            tscv = TimeSeriesSplit(n_splits=cv, max_train_size=60)
 
             gs = GridSearchCV(pipeline, params, cv=tscv, n_jobs=n_jobs,
                               verbose=verbose, scoring=self.scoring, refit=refit,
                               return_train_score=True)
             gs.fit(X, y)
             self.grid_searches[key] = gs
+
+    def fit_parallel(self, X, y, cv=3, n_jobs=3, verbose=1, refit=True, scoring=None):
+
+        ray.init()
+        list_of_keys = []
+        output = []
+        for key in self.keys:
+            print("Running GridSearchCV for %s." % key)
+
+            gs = run_model.remote(self.params[key], self.models[key], scoring, X, y, cv, n_jobs, verbose, refit)
+            list_of_keys.append(key)
+            output.append(gs)
+
+        output = ray.get(output)
+        self.grid_searches = {key: out for key ,out in zip(list_of_keys, output)}
 
     def score_summary(self):
         all_results = []
@@ -61,13 +77,27 @@ class EstimatorSelectionHelper:
 
         return results_df
 
-    def make_pipeline(self, model):
-        steps = list()
-        steps.append(('standardize', StandardScaler()))
-        steps.append(('normalize', MinMaxScaler()))
-        steps.append(('model', model))
-        # create pipeline
-        pipeline = Pipeline(steps=steps)
-        return pipeline
+def make_pipeline(model):
+    steps = list()
+    steps.append(('standardize', StandardScaler()))
+    steps.append(('normalize', MinMaxScaler()))
+    steps.append(('model', model))
+    # create pipeline
+    pipeline = Pipeline(steps=steps)
+    return pipeline
 
 
+@ray.remote(num_return_vals=1)
+def run_model(param, model, scoring, X, y, cv=3, n_jobs=3, verbose=1, refit=True):
+    multi_model = MultiOutputRegressor(model)
+
+    pipeline = make_pipeline(multi_model)
+    params = param
+    tscv = TimeSeriesSplit(n_splits=cv)
+
+    gs = GridSearchCV(pipeline, params, cv=tscv, n_jobs=n_jobs,
+                      verbose=verbose, scoring=scoring, refit=refit,
+                      return_train_score=True)
+    gs.fit(X, y)
+
+    return gs
