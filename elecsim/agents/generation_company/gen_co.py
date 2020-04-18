0379e6022be9f4c9138ba94996bec539cbf49388
Alexander Kell <alexander@kell.es>
2020-04-18 15:37:46 +0100

    fixing bugs

--- elecsim/agents/generation_company/gen_co.py
+++ elecsim/agents/generation_company/gen_co.py
@@ -208,10 +208,10 @@ class GenCo(Agent):
         print("action: {}".format(action))
         # plant_string_to_invest = plant_list[action.item(0)]
         number_of_plants = 1
+        action = action - 1
         if action > len(plant_list):
             number_of_plants = action//len(plant_list)
             action = action % len(plant_list)
-            # action = action - 1
 
         # print("len(plant_list): {}".format(len(plant_list)))
         # print("number_of_plants: {}".format(number_of_plants))
