1284189cec9cf204a02892877790e63f5d6c140c
Alexander Kell <alexander@kell.es>
2020-04-19 12:04:35 +0100

    began to compare day-ahead accuracy vs. cost and CO2 emissions

---
+++ run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
@@ -0,0 +1,10 @@
+"""
+File name: compare_by_ml_accuracy
+Date created: 19/04/2020
+Feature: #Enter feature description here
+"""
+
+__author__ = "Alexander Kell"
+__copyright__ = "Copyright 2018, Alexander Kell"
+__license__ = "MIT"
+__email__ = "alexander@kell.es"
