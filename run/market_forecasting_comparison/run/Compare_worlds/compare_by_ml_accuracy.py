e218ce6d700df07e5effe2935ce225af87f1c7d9
Alexander Kell <alexanderkell@alexanders-mbp.lan>
2020-04-29 16:11:41 +0100

    trying to fix bug

--- run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
+++ run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
@@ -56,16 +56,48 @@ number_of_steps = YEARS_TO_RUN * MARKET_TIME_SPLICES
 scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
 # scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
 
+@ray.remote
+def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fitting_params=None, optimal_carbon_tax=None):
+    beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
+    # beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.0021988838824299997, 12.633572943294599, 120.21276910611674, 0.0, 0.00059945111227]
+
+    prices_individual = np.array(beis_params[:-3]).reshape(-1, 2).tolist()
+
+    MARKET_TIME_SPLICES = 8
+    YEARS_TO_RUN = 17
+    number_of_steps = YEARS_TO_RUN * MARKET_TIME_SPLICES
+
+    scenario_2018 = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
+
+    # if individual[0] == 1:
+    #     individual = [individual[1]*i + individual[2] for i in range(1, 20)]
+    # else:
+    #     individual = [individual[3]*i ** individual[4] + individual[2] for i in range(1, 20)]
+    # individual = [individual[0]*i + individual[1] for i in range(1, 20)]
+    # print(individual)
+
+
+    world = World(carbon_price_scenario=optimal_carbon_tax, initialization_year=2018, scenario_file=scenario_2018, market_time_splices=MARKET_TIME_SPLICES, data_folder="compare_ml_accuracy", number_of_steps=number_of_steps, long_term_fitting_params=prices_individual, highest_demand=63910, nuclear_subsidy=beis_params[-3], future_price_uncertainty_m=beis_params[-2], future_price_uncertainty_c=beis_params[-1])
+    for _ in range(YEARS_TO_RUN):
+        for i in range(MARKET_TIME_SPLICES):
+            # try:
+            if i/8 == 0:
+                print('end of year')
+
+            average_electricity_price, carbon_emitted = world.step()
+
+
+
 # @ray.remote
-def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fitting_params=None, optimal_carbon_tax = None):
-    world = World(initialization_year=2018, market_time_splices=MARKET_TIME_SPLICES, data_folder="results", number_of_steps=number_of_steps, scenario_file=scenario_file, demand_distribution=demand_distribution, long_term_fitting_params=long_term_fitting_params, carbon_price_scenario = optimal_carbon_tax)
-    for i in range(num_steps):
-        world.step()
+# def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fitting_params=None, optimal_carbon_tax = None):
+#     world = World(initialization_year=2018, market_time_splices=MARKET_TIME_SPLICES, data_folder="results", number_of_steps=number_of_steps, scenario_file=scenario_file, demand_distribution=demand_distribution, long_term_fitting_params=long_term_fitting_params, carbon_price_scenario=optimal_carbon_tax)
+#     for i in range(num_steps):
+#         world.step()
 
 
 # results = []
 if __name__ == '__main__':
-    # ray.init(num_cpus=1)
+    ray.init(num_cpus=7)
 
     beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
 
@@ -88,13 +120,11 @@ if __name__ == '__main__':
         # print(resultant_dists)
 
         dist_class = eval(list(result_distributions_object[resultant_dists].fitted_param.keys())[0] + ".rvs")
-        dist_object = dist_class(*list(result_distributions_object[resultant_dists].fitted_param.values())[0],
-                                 size=50000)
+        dist_object = dist_class(*list(result_distributions_object[resultant_dists].fitted_param.values())[0], size=50000)
         print(dist_object)
         for j in range(150):
-
             # run_world.remote(number_of_steps, dist_object, prices_individual)
-            run_world(number_of_steps, dist_object, prices_individual, carbon_list)
+            run_world.remote(number_of_steps, dist_object, prices_individual, carbon_list)
 
         time.sleep(30)
 
