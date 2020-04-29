b1643ac0ebbae17ea88429cd8b835b2d03c35212
Alexander Kell <alexanderkell@alexanders-mbp.lan>
2020-04-29 18:13:53 +0100

    got working in parallel

--- run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
+++ run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
@@ -13,8 +13,10 @@ import sys
 import pickle
 import ray
 from elecsim.model.world import World
+from tqdm import tqdm
+
 import numpy as np
-import multiprocessing
+# import multiprocessing
 import tracemalloc
 from elecsim.constants import ROOT_DIR
 
@@ -57,7 +59,7 @@ scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".
 # scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
 
 # @ray.remote
-def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fitting_params=None, optimal_carbon_tax=None):
+def run_world(optimal_carbon_tax=None):
     beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
     # beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.0021988838824299997, 12.633572943294599, 120.21276910611674, 0.0, 0.00059945111227]
 
@@ -85,7 +87,7 @@ def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fit
                 print('end of year')
 
             average_electricity_price, carbon_emitted = world.step()
-
+    return None
 
 
 # @ray.remote
@@ -121,17 +123,12 @@ if __name__ == '__main__':
 
         dist_class = eval(list(result_distributions_object[resultant_dists].fitted_param.keys())[0] + ".rvs")
         dist_object = dist_class(*list(result_distributions_object[resultant_dists].fitted_param.values())[0], size=50000)
-        # print(dist_object)
-        # for j in range(150):
-        #     # run_world.remote(number_of_steps, dist_object, prices_individual)
-        #     # run_world.remote(number_of_steps, dist_object, prices_individual, carbon_list)
-        #     run_world(number_of_steps, dist_object, prices_individual, carbon_list)
-        # with multiprocessing.Pool(processes=7) as pool:
-        # Parallel
-        Parallel(n_jobs=7)(run_world(number_of_steps, dist_object, prices_individual, carbon_list) for i in range(150))
+
+
+        resList = Parallel(n_jobs=7)(delayed(run_world)(carbon_list) for i in tqdm(range(0, 100)))
             # pool.map(run_world(number_of_steps, dist_object, prices_individual, carbon_list), list(range(0, 150)))
 
-        time.sleep(30)
+        # time.sleep(30)
 
     os.execv(sys.executable, [sys.executable] + sys.argv)
 
