90bf0df582a586274d6264be2808b9cd75acd42f
Alexander Kell <alexanderkell@alexanders-mbp.lan>
2020-04-29 17:12:36 +0100

    put in parallel

--- run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
+++ run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
@@ -7,14 +7,14 @@ import sys
 from datetime import date, datetime, timedelta
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath('__file__')), '../../..'))
 ROOT_DIR = os.path.join(os.path.dirname(os.path.realpath('__file__')), '')
-
+from joblib import Parallel, delayed
 import os.path
 import sys
 import pickle
 import ray
 from elecsim.model.world import World
 import numpy as np
-
+import multiprocessing
 import tracemalloc
 from elecsim.constants import ROOT_DIR
 
@@ -56,7 +56,7 @@ number_of_steps = YEARS_TO_RUN * MARKET_TIME_SPLICES
 scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
 # scenario_file = "{}/../run/beis_case_study/scenario/reference_scenario_2018.py".format(ROOT_DIR)
 
-@ray.remote
+# @ray.remote
 def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fitting_params=None, optimal_carbon_tax=None):
     beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
     # beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.0021988838824299997, 12.633572943294599, 120.21276910611674, 0.0, 0.00059945111227]
@@ -97,7 +97,7 @@ def run_world(num_steps=number_of_steps, demand_distribution=None, long_term_fit
 
 # results = []
 if __name__ == '__main__':
-    ray.init(num_cpus=7)
+    # ray.init(num_cpus=7)
 
     beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
 
@@ -115,16 +115,21 @@ if __name__ == '__main__':
     carbon_list = carbon_df.x.tolist()
 
     result_distributions_object = pickle.load(open("{}/../run/market_forecasting_comparison/data/distribution_objects/result_distributions_object.p".format(ROOT_DIR), "rb"))
-    print(result_distributions_object)
+    # print(result_distributions_object)
     for resultant_dists in result_distributions_object:
         # print(resultant_dists)
 
         dist_class = eval(list(result_distributions_object[resultant_dists].fitted_param.keys())[0] + ".rvs")
         dist_object = dist_class(*list(result_distributions_object[resultant_dists].fitted_param.values())[0], size=50000)
-        print(dist_object)
-        for j in range(150):
-            # run_world.remote(number_of_steps, dist_object, prices_individual)
-            run_world.remote(number_of_steps, dist_object, prices_individual, carbon_list)
+        # print(dist_object)
+        # for j in range(150):
+        #     # run_world.remote(number_of_steps, dist_object, prices_individual)
+        #     # run_world.remote(number_of_steps, dist_object, prices_individual, carbon_list)
+        #     run_world(number_of_steps, dist_object, prices_individual, carbon_list)
+        # with multiprocessing.Pool(processes=7) as pool:
+        # Parallel
+        Parallel(n_jobs=7)(run_world(number_of_steps, dist_object, prices_individual, carbon_list) for i in range(150))
+            # pool.map(run_world(number_of_steps, dist_object, prices_individual, carbon_list), list(range(0, 150)))
 
         time.sleep(30)
 
