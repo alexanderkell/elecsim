f26f71a50db241160cc3558b3ce229670bf8d962
Alexander Kell <alexanderkell@alexanders-mbp.lan>
2020-05-03 15:31:06 +0100

    changed from loky to backend='multiprocessing'

--- run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
+++ run/market_forecasting_comparison/run/Compare_worlds/compare_by_ml_accuracy.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
         # print(dist_object))
         # run_world(carbon_list, resultant_dists, dist_object)
         # Parallel(n_jobs=multiprocessing.cpu_count()-1)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for i in tqdm(range(0, 100)))
-        Parallel(n_jobs=multiprocessing.cpu_count()-1, verbose=51)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for _ in range(0, 100))
+        Parallel(n_jobs=multiprocessing.cpu_count()-1, verbose=51, backend='multiprocessing')(delayed(run_world)(carbon_list, resultant_dists, dist_object) for _ in range(0, 100))
         # Parallel(n_jobs=7)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for i in tqdm(range(0, 100)))
         # pool.map(run_world(number_of_steps, dist_object, prices_individual, carbon_list), list(range(0, 150)))
 
