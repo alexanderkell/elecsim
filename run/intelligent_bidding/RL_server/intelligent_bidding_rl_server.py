7af1c238a2eb81a284354b1266a1bfbe8d14fb0e
Alexander Kell <alexander@kell.es>
2020-05-25 06:50:21 +0100

    working through experiments

--- run/intelligent_bidding/RL_server/intelligent_bidding_rl_server.py
+++ run/intelligent_bidding/RL_server/intelligent_bidding_rl_server.py
@@ -58,6 +58,7 @@ class MarketServing(ExternalEnv):
             # Discrete(159),
             # action_space=Box(shape=37),
             action_space=Box(low=0, high=200, shape=(37,), dtype=np.float),
+            # action_space=Box(low=0, high=200, shape=(25,), dtype=np.float),
             observation_space=Box(np.array(lower_bounds), np.array(upper_bounds))
         )
 
