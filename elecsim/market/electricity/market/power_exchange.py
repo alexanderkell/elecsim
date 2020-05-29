52af47e054088ac540152fc6de70ac2967a63365
Alexander Kell <alexander@kell.es>
2020-05-29 12:58:37 +0100

    running intelligent bidding and updating market comparison paper

--- elecsim/market/electricity/market/power_exchange.py
+++ elecsim/market/electricity/market/power_exchange.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 from itertools import chain
 # from linetimer import CodeTimer
 import pandas as pd
-from statistics import mean
+from statistics import mean, StatisticsError
 
 from elecsim.role.market.latest_market_data import LatestMarketData
 from elecsim.market.electricity.bid import Bid
@@ -92,9 +92,13 @@ class PowerExchange:
             highest_bid = self._accept_bids(accepted_bids)
 
             if self.model.gencos_rl:
-                total_accepted_bids = mean([int(rl_bid.bid_accepted)*rl_bid.price_per_mwh for rl_bid in accepted_bids if rl_bid.rl_bid is True])
+                try:
+                    average_accepted_price = mean([int(rl_bid.bid_accepted)*rl_bid.price_per_mwh for rl_bid in accepted_bids if rl_bid.rl_bid is True])
+                except StatisticsError:
+                    average_accepted_price = 0
+
                 # logger.info("total_accepted_bids: {}".format(total_accepted_bids))
-                self.model.bidding_client.log_returns(eid_bidding, total_accepted_bids)
+                self.model.bidding_client.log_returns(eid_bidding, average_accepted_price)
                 self.model.bidding_client.end_episode(eid_bidding, observation)
 
             if self.demand_distribution:
