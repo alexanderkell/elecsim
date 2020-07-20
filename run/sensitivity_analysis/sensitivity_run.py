f46e71b468693a9a0d0908c82776d99ef6c51cf2
Alexander Kell <alexander@kell.es>
2020-07-20 09:40:40 +0100

    working on sensitivity analysis

--- run/sensitivity_analysis/sensitivity_run.py
+++ run/sensitivity_analysis/sensitivity_run.py
@@ -40,12 +40,13 @@ if __name__ == '__main__':
     beis_params = [0.00121256259168, 46.850377392563864, 0.0029982421515, 28.9229765616468, 0.00106156336814, 18.370337670063762, 0.00228312539654, 0.0, 0.0024046471141100003, 34.43480109190594, 0.0, -20.88014916953091, 0.0, 8.15032953348701, 0.00200271495761, -12.546185375581802, 0.00155518243668, 39.791132970522796, 0.00027449937576, 8.42878689508516, 0.00111989525697, 19.81640207212787, 0.00224091998324, 5.26288570922149, 0.00209189353332, -5.9117317131295195, 0.00240696026847, -5.0144941135222, 0.00021183142492999999, -1.29658413335784, 0.00039441444392000004, -11.41659250225168, 0.00039441444392000004, -11.41659250225168, 120.21276910611674, 0.0, 0.00059945111227]
 
     if len(sys.argv) == 1:
-        data_folder = "/sensitivity_analysis/data/sensitivity_analysis_results"
+        data_folder = "/sensitivity_analysis/data/sensitivity_analysis_results/downpayment/wacc_059"
+
     else:
         data_folder = sys.argv[1]
 
     # path = '/Users/alexanderkell/Documents/PhD/Projects/10-ELECSIM/run/sensitivity_analysis/scenario_files/wacc/'  # use your path
-    path = '/Users/alexanderkell/Documents/PhD/Projects/10-ELECSIM/run/sensitivity_analysis/scenario_files/upfront_investment_costs/'  # use your path
+    path = '/Users/alexanderkell/Documents/PhD/Projects/10-ELECSIM/run/sensitivity_analysis/scenario_files/upfront_investment_costs'  # use your path
     all_files = glob.glob(path + "/*.py")
 
     scenario_files = []
