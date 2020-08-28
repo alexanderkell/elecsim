1882e1e7819898b829042ee6e2249353ba82be86
Alexander Kell <alexander@kell.es>
2020-08-28 16:34:21 +0100

    removing fluff

--- run/market_forecasting_comparison/run/sensitivity_analysis/sensitivity_analysis_distribution.py
+++ run/market_forecasting_comparison/run/sensitivity_analysis/sensitivity_analysis_distribution.py
@@ -151,7 +151,7 @@ if __name__ == '__main__':
         # print(dist_object))
         # run_world(carbon_list, resultant_dists, dist_object)
         # Parallel(n_jobs=multiprocessing.cpu_count()-1)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for i in tqdm(range(0, 100)))
-        returned_result = Parallel(n_jobs=multiprocessing.cpu_count()-1)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for _ in range(0, 100))
+        Parallel(n_jobs=multiprocessing.cpu_count()-1)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for _ in range(0, 100))
 
 
         # Parallel(n_jobs=7)(delayed(run_world)(carbon_list, resultant_dists, dist_object) for i in tqdm(range(0, 100)))
