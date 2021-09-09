4ab85a20b993a3ffd9448fed1f034166612acc8b
Alexander Kell <alexander@kell.es>
2021-09-09 15:24:21 +0100

    fixing bugs

--- run/run.py
+++ run/run.py
@@ -1,15 +1,16 @@
 import sys
+
 # import scenario_file
 # sys.modules['elecsim'].scenario.scenario_data=scenario_file
 
 from elecsim.model.world import World
 
 import logging
+
 # logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    world = World(2018, log_level="info", market_time_splices=8)
-    for year in range(20):
-        for day in range(8):
-            world.step()
+    world = World(2018, log_level="info", market_time_splices=8, number_of_steps=8)
+    for day in range(8):
+        world.step()
